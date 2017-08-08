5638b07a642bb78bf184fef370717d3f81412d16
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-08-08 12:36:42 +0900

    pushed data from sqlite to postgresql via dump

--- db/migrate/20170804080907_drop_orders.rb
+++
@@ -1,5 +0,0 @@
-class DropOrders < ActiveRecord::Migration[5.0]
-  def up
-    drop_table :orders
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "Fontainebleau", :artist => "Nam June Paik", :image_url => 'njp.png')
