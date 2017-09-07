16b729abbeb3f9282656a06f8f443e45907b3e31
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-09-07 14:03:51 +0900

    try out new redis.rb settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,2 @@
-if Rails.env.production?
-  $redis = Redis.new(url: ENV["REDIS_URL"])
-  else
-  $redis = Redis.new(host: 'localhost', port: 6379)
-end
 uri = URI.parse(ENV.fetch("REDISTOGO_URL", "redis://redistogo:74fef00d09bf0fe0143910314a58b0cb@angelfish.redistogo.com:11784/"))
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
