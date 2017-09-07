5903f8dff9226c37625637d8edc6fc4b370600f5
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-09-07 13:58:00 +0900

    updated settings to redis to go

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,3 +3,5 @@ if Rails.env.production?
   else
   $redis = Redis.new(host: 'localhost', port: 6379)
 end
+uri = URI.parse(ENV.fetch("REDISTOGO_URL", "redis://redistogo:74fef00d09bf0fe0143910314a58b0cb@angelfish.redistogo.com:11784/"))
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
